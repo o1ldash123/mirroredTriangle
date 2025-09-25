b9d08851b9f10d0aff44df21ec9a48834fc66025
o1ldash123 <adekunbibabayomi@gmail.com>
2025-09-24 19:38:33 -0600

    mirrored right angle triangle

--- pyramid.py
+++ pyramid.py
@@ -1,19 +1,10 @@
+rows = int(input("Enter the number of rows: "))
 
-rowSize = int(input("Enter the number of rows: "))
-
-if rowSize % 2 == 0:
-    halfDiamRow = int(rowSize / 2)
-else:
-    halfDiamRow = int(rowSize/2) + 1
-space = halfDiamRow - 1
-
-for i in range(1 , halfDiamRow + 1):
-  for j in range(1 , space + 1):
-    print(end= ' ')
-  space -= 1
-  num = 1
-  for j in range(2  * i - 1):
-     print(end = str(num))
-     num += 1
-  print()
-space = 1
+for i in range(1, rows + 1):
+        # Print leading spaces
+        for _ in range(rows - i):
+            print(" ", end="")
+        # Print asterisks
+        for _ in range(i):
+            print("*", end="")
+        print() 
